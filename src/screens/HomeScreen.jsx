5f79145322accd47f8fe7d9ff4f86912f6199f36
Amanda <eunumseioqpordeemail@gmail.com>
2023-06-28 14:02:25 -0300

    cardYay

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import MyComponent from "./cardTeste";
 
 export default function HomeScreen() {
     return (
         <View>
-            <Text>Home.</Text>
+            <MyComponent />
         </View>      
     )
 }
+
