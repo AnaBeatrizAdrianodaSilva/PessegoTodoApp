7fd59d858b5acd4234ddab3985c4b4cba8931728
Amanda <eunumseioqpordeemail@gmail.com>
2023-06-28 15:56:17 -0300

    aaaaaaa

--- src/screens/EditarTaskScreen.jsx
+++ src/screens/EditarTaskScreen.jsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 
-export default function EditarTaskScreen({ navigation }) {
+export default function EditarTaskScreen() {
     return (
         <View>
             <Text>Editar Task.</Text>
