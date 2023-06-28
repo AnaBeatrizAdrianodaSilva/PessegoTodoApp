9f7343eee7bdc89116a73616c1992daa4d004dac
Ana Beatriz <ana.github0@gmail.com>
2023-06-28 14:19:01 -0300

    adicionar task pronta

--- src/screens/AddTaskScreen.jsx
+++ src/screens/AddTaskScreen.jsx
@@ -1,21 +1,38 @@
 import { View } from "react-native";
 import { Button, TextInput } from "react-native-paper";
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../config/firebase";
 
 export default function AddTaskScreen() {
-    return (
-        <View>
-            <View>
-                <TextInput
-                    placeholder="Title"
-                />
-                <TextInput
-                    placeholder="Content"
-                />
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-                <Button
-                    mode="contained"
-                >Adicionar Task</Button>
-            </View>
-        </View>      
-    )
+
+  async function inserirTask() {
+    try {
+      const taskRef = collection(db, "Tasks");
+      const payload = {
+        Title: title,
+        Content: content,
+      };
+      const task = await addDoc(taskRef, payload);
+      console.log(task);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <View>
+      <View>
+        <TextInput label="Titulo" value={title} onChangeText={setTitle} />
+        <TextInput label="Descrição" value={content} onChangeText={setContent} />
+
+        <Button mode="contained" onPress={inserirTask}>
+          Adicionar Task
+        </Button>
+      </View>
+    </View>
+  );
 }
